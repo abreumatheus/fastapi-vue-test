d89868a9fcb21e4dd95f0d5a72514fcd74121710
Matheus Abreu <abreumatheus@icloud.com>
2020-05-14 19:55:37 -0300

    Added new util method to avoid duplication

--- fastapi/app/utils/__init__.py
+++ fastapi/app/utils/__init__.py
@@ -27,6 +27,15 @@ def string_to_list(link: str):
 	return link
 
 
+def cleanup_dict_and_title_strings(dictionary: dict) -> dict:
+	for item in list(dictionary):
+		if dictionary[item] is None:
+			del dictionary[item]
+		elif isinstance(dictionary[item], str):
+			dictionary[item] = dictionary[item].title()
+	return dictionary
+
+
 if __name__ == '__main__':
 	public_id = 'SUL' + ran_gen(6, "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
 	print(public_id)
