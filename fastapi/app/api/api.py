a47e35efc79031df78b1cc01f687811e13f678f6
Matheus Abreu <abreumatheus@icloud.com>
2020-05-14 21:32:54 -0300

    Registered categories endpoint

--- fastapi/app/api/api.py
+++ fastapi/app/api/api.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 
 from app.api.endpoints import products
+from app.api.endpoints import categories
 
 api_router = APIRouter()
 api_router.include_router(products.router, prefix="/products", tags=["Products"])
+api_router.include_router(categories.router, prefix="/categories", tags=["Categories"])
